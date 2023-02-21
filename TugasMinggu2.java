38f6f979c22bee36011a5d9edae7dac530998570
tegar148 <124846235+tegar148@users.noreply.github.com>
2023-02-21 08:31:53 +0700

    Add files via upload

---
+++ TugasMinggu2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas.minggu.pkg2;
+
+/**
+ *
+ * @author macbookpro
+ */
+public class TugasMinggu2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "tegar malik";
+        String Stid = "e41220670";
+        String ss = "active";
+        System.out.println("Student name:" +nama) ;
+        System.out.println("student id:" +Stid);
+        System.out.println("student status:"+ss);
+        // TODO code application logic here
+    }
+    
+}
